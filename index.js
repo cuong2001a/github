e74d363f1bb1c34154b321d3af7017c79e3afe36
Ngo viet cuong <vietcuong11a22001@gmail.com>
2021-03-15 22:11:24 +0700

    git itignore

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ confirm("hello");
 ,,
 sdfsdfsdf
 git diff 
-git reset Head 
-git revert
-.gitignore
+git reset Head ||-- soft: quay lai luc duoc add roi ||--mixed :quay lai luc chua duoc add|| --hard: + id 
+git revert thay doi code 
+.gitignore : loai bo nhung file ma mk khong muon upload len
