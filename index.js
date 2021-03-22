bf3788834319ca4c8bf6304b7b66f576d75489ee
Ngo viet cuong <vietcuong11a22001@gmail.com>
2021-03-22 21:24:48 +0700

    aaaa

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-console.log("Team web training");
-kkll,
-
-confirm("hello");
-,,
-sdfsdfsdf
-git diff 
-git reset Head ||-- soft: quay lai luc duoc add roi ||--mixed :quay lai luc chua duoc add|| --hard: + id 
-git revert thay doi code 
-.gitignore : loai bo nhung file ma mk khong muon upload len
+dsfsdfsdfsdfsdfsdasdjfkasdfhasjdfasdjkfhaskjdfh
